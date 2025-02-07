32ad97f443d17c021a6da4a4f94f4a19a33127d8
IQBit <iqnitetechnologies@gmail.com>
2025-02-07 18:21:27 +0100

    Format

--- app.py
+++ app.py
@@ -50,7 +50,9 @@ eggsplode_app = Eggsplode(
 
 logger = logging.getLogger("discord")
 if LOG_PATH:
-    handler = RotatingFileHandler(LOG_PATH, maxBytes=5 * 1024 * 1024, backupCount=5, encoding='utf-8')
+    handler = RotatingFileHandler(
+        LOG_PATH, maxBytes=5 * 1024 * 1024, backupCount=5, encoding="utf-8"
+    )
     formatter = logging.Formatter(
         "%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
