64b95da53dfa6b0bb156790cc813685c9ebc89a5
IQBit <iqnitetechnologies@gmail.com>
2025-02-26 14:09:58 +0100

    Fix runtime warning

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ async def start(ctx: discord.ApplicationContext):
     Args:
         ctx (discord.ApplicationContext): The context of the command.
     """
-    ctx.response.defer()
+    await ctx.response.defer()
     cleanup()
     if eggsplode_app.admin_maintenance:
         await ctx.respond(MESSAGES["maintenance"], ephemeral=True)
@@ -136,7 +136,7 @@ async def hand(ctx: discord.ApplicationContext):
     Args:
         ctx (discord.ApplicationContext): The context of the command.
     """
-    ctx.response.defer()
+    await ctx.response.defer()
     game_id = ctx.interaction.channel_id
     if not (game_id and ctx.interaction.user):
         return
@@ -174,7 +174,7 @@ async def games(ctx: discord.ApplicationContext):
     Args:
         ctx (discord.ApplicationContext): The context of the command.
     """
-    ctx.response.defer()
+    await ctx.response.defer()
     cleanup()
     if not ctx.interaction.user:
         return
@@ -206,7 +206,7 @@ async def show_help(ctx: discord.ApplicationContext):
     Args:
         ctx (discord.ApplicationContext): The context of the command.
     """
-    ctx.response.defer()
+    await ctx.response.defer()
     await ctx.respond(
         "\n".join(MESSAGES["help"]).format(
             eggsplode_app.latency * 1000,
@@ -253,7 +253,7 @@ async def bugreport(ctx: discord.ApplicationContext, bug_type: str, description:
         bug_type (str): The type of bug being reported.
         description (str): A detailed description of the bug.
     """
-    ctx.response.defer()
+    await ctx.response.defer()
     if not ctx.interaction.user:
         return
     logger.info(
@@ -289,7 +289,7 @@ async def admincmd(
         ctx (discord.ApplicationContext): The context of the command.
         command (str): The admin command to execute.
     """
-    ctx.response.defer()
+    await ctx.response.defer()
     if command == ADMIN_MAINTENANCE_CODE:
         cleanup()
         eggsplode_app.admin_maintenance = not eggsplode_app.admin_maintenance
