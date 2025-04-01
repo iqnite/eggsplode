52d150824c328ec527f9ef8cca1061e32aac5d16
iqnite <iqnitetechnologies@gmail.com>
2025-04-01 13:28:51 +0200

    Fix

--- eggsplode/commands.py
+++ eggsplode/commands.py
@@ -2,16 +2,13 @@
 Contains the commands for the Eggsplode game.
 """
 
-import asyncio
 from datetime import datetime
-import os
 import discord
 from discord.ext import commands
 
 
 from .game_logic import Game
 from .strings import VERSION, get_message
-from .views.base import ButtonView
 from .views.starter import HelpView
 
 
@@ -20,9 +17,9 @@ class EggsplodeApp(commands.Bot):  # pylint: disable=too-many-ancestors
         super().__init__(**kwargs)
         self.admin_maintenance: bool = False
         self.games: dict[int, Game] = {}
-        self.load_extension("cogs.eggsplode_game")
-        self.load_extension("cogs.mini_games")
-        self.load_extension("cogs.misc")
+        self.load_extension("eggsplode.cogs.eggsplode_game")
+        self.load_extension("eggsplode.cogs.mini_games")
+        self.load_extension("eggsplode.cogs.misc")
 
     def games_with_user(self, user_id: int) -> list[int]:
         return [i for i, game in self.games.items() if user_id in game.players]
