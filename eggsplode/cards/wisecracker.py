d0625aaf3539d0cf3f0a908e0b8879ca865e4c5e
iqnite <iqnitetechnologies@gmail.com>
2025-06-15 13:45:51 +0200

    Add Bury

--- eggsplode/cards/wisecracker.py
+++ eggsplode/cards/wisecracker.py
@@ -5,7 +5,7 @@ Contains card effects for the Wise-Cracker expansion.
 from typing import TYPE_CHECKING
 import discord
 from eggsplode.cards.base import game_over, skip, attegg_finish
-from eggsplode.ui import ChoosePlayerView
+from eggsplode.ui import ChoosePlayerView, DefuseView
 from eggsplode.ui.base import TextView
 
 if TYPE_CHECKING:
@@ -31,34 +31,28 @@ async def wisecracker(game: "Game", interaction: discord.Interaction):
         await wisecracker_finish(game, interaction, players_with_wisecracker[0])
         return
     game.current_player_hand.append("wisecracker")
-    await game.send(
-        view=TextView("wisecracker_exposed", game.current_player_id), anchor=interaction
-    )
+    await game.send(view=TextView("wisecracker_exposed", game.current_player_id))
     await game.events.action_end()
 
 
-async def wisecracker_finish(
-    game: "Game", interaction: discord.Interaction, target_player_id: int
-):
+async def wisecracker_finish(game: "Game", _, target_player_id: int):
     if "defuse" in game.hands[target_player_id]:
         game.hands[target_player_id].remove("defuse")
         await game.send(
             view=TextView(
                 "wisecracker_defused", game.current_player_id, target_player_id
             ),
-            anchor=interaction,
         )
     else:
         await game.send(
             view=TextView(
                 "wisecracker_eggsploded", game.current_player_id, target_player_id
             ),
-            anchor=interaction,
         )
         del game.players[game.players.index(target_player_id)]
         del game.hands[target_player_id]
         if len(game.players) == 1:
-            await game_over(game, interaction)
+            await game_over(game, _)
             return
     await game.events.action_end()
 
@@ -72,6 +66,20 @@ async def self_attegg(game: "Game", _):
     await attegg_finish(game, game.current_player_id, turns=4)
 
 
+async def bury(game: "Game", interaction: discord.Interaction):
+    view = DefuseView(
+        game,
+        lambda: bury_finish(game),
+        card=game.deck.pop(),
+    )
+    await interaction.respond(view=view, ephemeral=True)
+
+
+async def bury_finish(game: "Game"):
+    await game.send(view=TextView("buried", game.current_player_id))
+    await game.events.turn_end()
+
+
 def setup(game: "Game"):
     game.deck += ["wisecracker"] * 2
 
@@ -80,6 +88,7 @@ PLAY_ACTIONS = {
     "wisecracker": wisecracker,
     "super_skip": super_skip,
     "self_attegg": self_attegg,
+    "bury": bury,
 }
 
 SETUP_ACTIONS = [
