1060a94fb1b0f085f2d832070b36f231b421cb4f
iqnite <iqnitetechnologies@gmail.com>
2025-09-19 14:34:07 +0200

    Remove commented lines

--- eggsplode/ui/turn.py
+++ eggsplode/ui/turn.py
@@ -41,8 +41,3 @@ class TurnView(BaseView):
         self.stop()
         self.game.events.turn_end -= self.deactivate
         self.game.events.game_end -= self.deactivate
-        # self.disable_all_items()
-        # for item in (self.draw_button, self.play_button, self.warnings):
-        #     self.remove_item(item)
-        # if self.game.anchor_interaction:
-        #     await self.game.anchor_interaction.edit_original_response(view=self)
