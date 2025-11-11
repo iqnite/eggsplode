f5b395c4c5cfe804a45fc1fe084fc39bf603c9e8
iqnite <iqnitetechnologies@gmail.com>
2025-11-11 12:01:58 +0100

    Separate logging

--- app.py
+++ app.py
@@ -10,12 +10,8 @@ import discord
 from eggsplode.commands import EggsplodeApp
 from eggsplode.strings import discord_token, app_config, app_info
 
-if not discord_token:
-    raise TypeError("DISCORD_TOKEN must be set in .env file.")
 
-logger = logging.getLogger("discord")
-log_path = app_config.get("log_path", "")
-if log_path != "":
+def configure_logger(logger: logging.Logger, log_level_key: str):
     handler = RotatingFileHandler(
         log_path,
         maxBytes=int(app_config.get("log_bytes", 5242880)),  # Default 5 MB
@@ -28,19 +24,32 @@ if log_path != "":
     )
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+    default_log_level = app_config.get("log_level", "INFO")
     logger.setLevel(
-        getattr(logging, app_config.get("log_level", "INFO").upper(), logging.INFO)
+        getattr(
+            logging,
+            app_config.get(log_level_key, default_log_level).upper(),
+            logging.INFO,
+        )
     )
+
+
+if not discord_token:
+    raise TypeError("DISCORD_TOKEN must be set in .env file.")
+
+discord_logger = logging.getLogger("discord")
+app_logger = logging.getLogger("eggsplode")
+log_path = app_config.get("log_path", "")
+if log_path != "":
+    configure_logger(discord_logger, "discord_log_level")
+    configure_logger(app_logger, "app_log_level")
 
 status_activity = discord.CustomActivity(
     name=app_config.get("custom_status", "/start"),
 )
-app = EggsplodeApp(
-    activity=status_activity,
-    logger=logger,
-)
+app = EggsplodeApp(activity=status_activity, logger=app_logger)
 
 if __name__ == "__main__":
     if log_path != "":
-        logger.info("Program version %s started.", app_info["version"])
+        app_logger.info("Program version %s started.", app_info["version"])
     app.run(discord_token)
