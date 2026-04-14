cec8d4f0bc03f820184a51ee8dc915cf93cb3a61
iqnite <iqnitetechnologies@gmail.com>
2026-04-14 10:02:31 +0200

    Format

--- eggsplode/ui/start.py
+++ eggsplode/ui/start.py
@@ -470,9 +470,7 @@ class InfoView(discord.ui.DesignerView):
         self.container.add_separator()
         self.container.add_section(
             discord.ui.TextDisplay(
-                format_message(
-                    "status_installs", len(self.app.guilds)
-                )
+                format_message("status_installs", len(self.app.guilds))
             ),
             accessory=discord.ui.Button(
                 label="Install",
